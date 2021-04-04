c243c0962f944bfd93eb8d66ae6de6760d22b7ee
applandsys <applandsys@gmail.com>
2021-04-05 01:36:13 +0600

    Settings kora hoy nai akhono baki all okay ase

--- appjs/config.js
+++ appjs/config.js
@@ -1,4 +1,5 @@
-var app_config = {'version':1,'base_url':'https://promovaly.com/api/v1/','token':null};
+const app_config = {'version':1,'base_url':'https://promovaly.com/api/v1/','token':null};
+//const app_config = {'version':1,'base_url':'http://localhost/armover/api/v1/','token':null};
 
 
 
--- appjs/main.js
+++ appjs/main.js
@@ -1,6 +1,44 @@
 app_config.token = localStorage.getItem("token");
 
-console.log(app_config);
+//console.log(app_config.token);
+
+let authcheck = function(){
+	
+					var request_data = {'action':'authcheck'};
+					
+					$.ajax({
+						type:'POST',
+						url:app_config.base_url +"user/AuthCheck",
+						data:request_data,
+						headers: {"Authorization": "Bearer "+app_config.token},
+						dataType: 'json',
+						beforeSend: function(){
+							showModal() ;
+						},
+						complete: function(){
+							hideModal();
+						},
+						success: function(data){
+							if(data.status=='invalid'){
+								localStorage.removeItem("token");
+								app_config.token = null;
+								window.location.replace("index.html");
+							}else{
+								app_config.token = localStorage.getItem("token");	
+							}	
+						},
+						error: function(data){
+							ons.notification.alert({
+										title: 'Sorry!',
+										message: 'Internet Connection Problem'
+									});
+						}
+						
+					}); 
+}
+
+
+
 
 let orderPage = function(category){
 	var data = {data: {title: 'Order',category:category}}
@@ -12,6 +50,9 @@ document.addEventListener('init', function(event) {
 	
   var page = event.target ;
 // order //	
+	authcheck();
+
+
   if (page.matches('#order_info')) {
 	  
 	  	$("#signup_form").on('submit',function(e) {
@@ -83,4 +124,90 @@ document.addEventListener('init', function(event) {
 		
 		 
 	}// end order
+	
+	
+	if (page.matches('#order_list')) {
+	   
+			var request_data = {'action':'order_list'};
+					
+			$.ajax({
+				type:'POST',
+				url:app_config.base_url +"request/order_list",
+				data:request_data,
+				headers: {"Authorization": "Bearer "+app_config.token},
+				dataType: 'json',
+				beforeSend: function(){
+					showModal() ;
+				},
+				complete: function(){
+					hideModal();
+				},
+				success: function(data){
+					console.log(data);
+					var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+					var table = '';			
+						$.each(data.order_list, function (idx, obj) {      						
+							table += '<table id="customers">';
+							table += '<tr><th>Category</th><td>'+obj.category.category_name+'</td></tr>';
+							table += '<tr><th>From Address</th><td>'+obj.from_location+'</td></tr>';
+							table += '<tr><th>To Address</th><td>'+obj.to_location+'</td></tr>';
+							table += '<tr><th>Order Date</th><td>'+new Date(obj.created_at).toLocaleString('en-US', { timeZone: 'BST' },options)+'</td></tr>';
+							table += '</table><hr/>';
+						});   
+						
+						
+					$("#ordr_list").html(table);
+				},
+				error: function(data){
+					ons.notification.alert({
+								title: 'Sorry!',
+								message: 'Internet Connection Problem'
+							});
+				}
+				
+			}); 
+	  			
+	}// end order
+	
+	
+	if (page.matches('#about')) {
+	   
+							
+			$.ajax({
+				type:'GET',
+				url:app_config.base_url +"request/about",
+				headers: {"Authorization": "Bearer "+app_config.token},
+				dataType: 'json',
+				beforeSend: function(){
+					showModal() ;
+				},
+				complete: function(){
+					hideModal();
+				},
+				success: function(data){
+					console.log(data);
+					 
+						
+						
+					$("#title").html(data.about[0].title);
+					$("#detail").html(data.about[0].content);
+				},
+				error: function(data){
+					ons.notification.alert({
+								title: 'Sorry!',
+								message: 'Internet Connection Problem'
+							});
+				}
+				
+			}); 
+	  			
+	}// end about
+	
+	if (page.matches('#settings')) {
+	   
+							
+	
+	  			
+	}// end setting
+ 
 }, false);	
